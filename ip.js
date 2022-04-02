024ae6e5447d5d18456b84a488cb2aa1f5a006d5
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-04-02 23:03:23 +0600

    Add files via upload

---
+++ ip.js
@@ -0,0 +1,22 @@
+const puppeteer = require("puppeteer")
+
+
+;(async () => {
+
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
+
+    const page = (await browser.pages())[0]
+
+    await page.goto('https://ifconfig.me/ip', {waitUntil: 'domcontentloaded', timeout: 0})
+
+    const ip = await page.evaluate(() => document.querySelector('pre').innerHTML )
+
+    console.log(ip)
+
+    page.close()
+
+    browser.close()
+})()
