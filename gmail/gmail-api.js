73662e8c960205fe8cb893397fa658c8837655aa
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-05-12 04:06:38 +0600

    Add files via upload

--- gmail/gmail-api.js
+++ gmail/gmail-api.js
@@ -119,7 +119,7 @@ async function startService() {
     ;(async () => {
 
         browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             args: [ '--no-sandbox', '--disable-setuid-sandbox' ]
         })
 
