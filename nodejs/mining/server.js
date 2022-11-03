783e651eaca0605a47395d6e7bcb06d664360ada
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-11-04 04:10:31 +0600

    Update server.js

--- nodejs/mining/server.js
+++ nodejs/mining/server.js
@@ -1,5 +1,14 @@
 const puppeteer = require("puppeteer")
 
+
+let url ='https://colab.research.google.com/drive/'
+let colab1 = '1aEU1oKekxX4_NyqCCx8hWUSOyYoEdKJi'
+let colab2 = '1qi-5bFKjDXnoh33AVGQgKIdBTpYxJH7s'
+let colab3 = '1PvtWshX9XPoif-BZKfSN1KQCDhe9hB8R'
+let colab4 = '1suavUZtqZutUb0AXWGFqwKfxE3IsSR8P'
+let colab5 = '1Zoo8FZ8CyTnjUGfVDkEpIRlFZfjGT6tC'
+
+
 let cookies = [
     {
       name: 'MoneroOceanAddr',
@@ -18,6 +27,8 @@ let cookies = [
   ]
 
 ;(async () => {
+    let mSize = 10
+    
     let browser = await puppeteer.launch({
         executablePath : "/usr/lib/chromium-browser/chromium-browser",
         //headless: false,
@@ -37,8 +48,34 @@ let cookies = [
 
     console.log('Page Load Start')
 
-    await page.goto('https://colab.research.google.com/drive/1vKu6N9ZfG0H9t8oe1sAkMgoB387cSr1p')
+    page.goto(url+colab1+'?authuser=0', { waitUntil: 'domcontentloaded', timeout: 0 })
 
+        for(let i=2; i<=mSize; i++) {
+            let colab = null
+            if(i == 1 || i == 6) {
+                colab = colab1
+            } else if(i == 2 || i == 7) {
+                colab = colab2
+            } else if(i == 3 || i == 8) {
+                colab = colab3
+            } else if(i == 4 || i == 9) {
+                colab = colab4
+            } else if(i == 5 || i == 10) {
+                colab = colab5
+            }
+            page = await browser.newPage()
+            map = {}
+            map['page'] = page
+            map['load'] = false
+            map['status'] = 0
+            pages[i] = map
+            if(i > 5) {
+                page.goto(url+colab+'?authuser=1', { waitUntil: 'domcontentloaded', timeout: 0 })
+            } else {
+                page.goto(url+colab+'?authuser=0', { waitUntil: 'domcontentloaded', timeout: 0 })
+            }
+        }
+    
     console.log('Page Load Success')
 
 
