6f215a34b0b4e5d40ea33986c3ab0c9ff1a659ee
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-11-04 11:06:52 +0600

    Update server.js

--- nodejs/mining/server.js
+++ nodejs/mining/server.js
@@ -1,82 +1,29 @@
 const puppeteer = require("puppeteer")
+const COLAB = require('./colab-api')
 
-
-let url ='https://colab.research.google.com/drive/'
-let colab1 = '1aEU1oKekxX4_NyqCCx8hWUSOyYoEdKJi'
-let colab2 = '1qi-5bFKjDXnoh33AVGQgKIdBTpYxJH7s'
-let colab3 = '1PvtWshX9XPoif-BZKfSN1KQCDhe9hB8R'
-let colab4 = '1suavUZtqZutUb0AXWGFqwKfxE3IsSR8P'
-let colab5 = '1Zoo8FZ8CyTnjUGfVDkEpIRlFZfjGT6tC'
-
-let DATA = null
-let mGmail = null
-let mId = 1
-let mLoadSuccess = false
-let temp = []
 let cookies = []
 
-let browser = null
 let pages = {}
-
 
 
 ;(async () => {
     let mSize = 10
     
-    browser = await puppeteer.launch({
+    let browser = await puppeteer.launch({
         executablePath : "/usr/lib/chromium-browser/chromium-browser",
         //headless: false,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     })
 
-    let page = (await browser.pages())[0]
-        let map = {}
-        map['page'] = page
-        map['load'] = false
-        map['status'] = 0
-        pages[1] = map
+    let mColab = new COLAB(browser)
 
-    await page.setCookie(...cookies)
+    for(let i=1; i<=mSize; i++) {
+        await mColab.newPage((data, position) => {
+            pages[position] = data
+        }, cookies, i)
+    }
 
-    page.on('console', async (msg) => {
-        const msgArgs = msg.args()
-        for (let i = 0; i < msgArgs.length; ++i) {
-          console.log(await msgArgs[i].jsonValue())
-        }
-    })
-
-    console.log('Page Load Start')
-
-    page.goto(url+colab1+'?authuser=0', { waitUntil: 'domcontentloaded', timeout: 0 })
-
-        for(let i=2; i<=mSize; i++) {
-            let colab = null
-            if(i == 1 || i == 6) {
-                colab = colab1
-            } else if(i == 2 || i == 7) {
-                colab = colab2
-            } else if(i == 3 || i == 8) {
-                colab = colab3
-            } else if(i == 4 || i == 9) {
-                colab = colab4
-            } else if(i == 5 || i == 10) {
-                colab = colab5
-            }
-            page = await browser.newPage()
-            map = {}
-            map['page'] = page
-            map['load'] = false
-            map['status'] = 0
-            pages[i] = map
-            if(i > 5) {
-                page.goto(url+colab+'?authuser=1', { waitUntil: 'domcontentloaded', timeout: 0 })
-            } else {
-                page.goto(url+colab+'?authuser=0', { waitUntil: 'domcontentloaded', timeout: 0 })
-            }
-        }
-    
-    console.log('Page Load Success')
-
+    console.log('load Success')
 
 })()
 
