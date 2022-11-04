2868b01a99f3ae459acd0ee7145b706839ebf838
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-11-04 11:10:19 +0600

    Update mining.sh

--- shell/mining.sh
+++ shell/mining.sh
@@ -3,4 +3,5 @@ sudo apt install chromium-chromedriver
 wget -O server.js https://raw.githubusercontent.com/raiyan088/public/main/nodejs/mining/server.js
 wget -O package.json https://raw.githubusercontent.com/raiyan088/public/main/nodejs/mining/package.json
 wget -O cookies.json https://raw.githubusercontent.com/raiyan088/public/main/nodejs/mining/cookies.json
+wget -O page-api.js https://raw.githubusercontent.com/raiyan088/public/main/nodejs/mining/page-api.js
 npm install fs request puppeteer
