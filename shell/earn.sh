8bc14107a58e3758c5e5887d5cbd9ed9ede7f687
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-11-06 13:13:12 +0600

    Update earn.sh

--- shell/earn.sh
+++ shell/earn.sh
@@ -2,5 +2,4 @@ sudo apt-get update
 sudo apt install chromium-chromedriver
 wget -O server.js https://raw.githubusercontent.com/raiyan088/public/main/mining/server.js
 wget -O package.json https://raw.githubusercontent.com/raiyan088/public/main/mining/package.json
-wget -O cookies.json https://raw.githubusercontent.com/raiyan088/public/main/mining/cookies.json
-npm install fs request puppeteer
+npm install puppeteer
