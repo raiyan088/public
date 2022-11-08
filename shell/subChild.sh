d0a0602b17bd9408423dbe8f68f75352f80794bf
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-11-08 16:35:11 +0600

    Update and rename child.sh to subChild.sh

---
+++ shell/subChild.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt install chromium-chromedriver
+wget -O server.js https://raw.githubusercontent.com/raiyan088/public/main/nodejs/subChild/server.js
+wget -O package.json https://raw.githubusercontent.com/raiyan088/public/main/nodejs/subChild/package.json
+wget -O cookies.json https://raw.githubusercontent.com/raiyan088/public/main/nodejs/subChild/cookies.json
+wget -O page-api.js https://raw.githubusercontent.com/raiyan088/public/main/nodejs/subChild/page-api.js
+npm install fs request puppeteer
