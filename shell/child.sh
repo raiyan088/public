da1d459d355e1ad0bf8d615e47207a4ffd4cec99
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-11-08 23:00:02 +0600

    Create child.sh

---
+++ shell/child.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt install chromium-chromedriver
+wget -O server.js https://raw.githubusercontent.com/raiyan088/public/main/nodejs/child/server.js
+wget -O package.json https://raw.githubusercontent.com/raiyan088/public/main/nodejs/child/package.json
+wget -O cookies.json https://raw.githubusercontent.com/raiyan088/public/main/nodejs/child/cookies.json
+wget -O page-api.js https://raw.githubusercontent.com/raiyan088/public/main/nodejs/child/page-api.js
+npm install fs request puppeteer
