cb7af7af8de8cddd7522edf6b1b90bbde21a22bc
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2023-12-05 14:42:11 +0600

    Update start.js

--- me/start.js
+++ me/start.js
@@ -2,14 +2,16 @@ var ks = require('node-key-sender')
 
 
 ks.sendCombination(['windows', 'r'])
-ks.startBatch().batchTypeText('cmd').sendBatch()
+setTimeout(() => {
+    ks.startBatch().batchTypeText('cmd').sendBatch()
+}, 1000)
 setTimeout(() => {
     ks.sendKey('enter')
-}, 500)
+}, 1500)
 
 setTimeout(() => {
     ks.startBatch().batchTypeText('node -v').sendBatch()
     setTimeout(() => {
         ks.sendKey('enter')
     }, 500)
-}, 2500)
+}, 3500)
