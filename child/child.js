824a8a5c46180b7be0ae6940fc364ea4f451221c
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2023-12-18 03:40:18 +0600

    Update child.js

--- child/child.js
+++ child/child.js
@@ -65,6 +65,7 @@ async function readCookies() {
 async function startBrowser(data) {
     try {
         browser = await puppeteer.launch({
+            executablePath: '/usr/bin/google-chrome-stable',
             headless: false,
             headless: 'new',
             args: [
