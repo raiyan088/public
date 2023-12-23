adfc8d8d2ecf9829d174a27ccfa91c4b694adaf7
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2023-12-24 04:55:06 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const SYMBLE = '#'
 const LENGTH = 3
 
 let SIZE = 0
+let EXTRA = '0'
 
 let load02 = true
 let load03 = true
@@ -23,6 +24,10 @@ process.argv.slice(2).forEach(function (data, index) {
             console.log('★★★---START---★★★')
 
             connect01()
+        } else if(index == 1) {
+            if (data == '1' || data == 1) {
+                EXTRA = '1'
+            }
         }
     } catch (error) {
         console.log('Index Error:', error)
@@ -30,7 +35,7 @@ process.argv.slice(2).forEach(function (data, index) {
 })
 
 async function connect01() {
-    process01 = exec('node server.js '+(SIZE+1))
+    process01 = exec('node server.js '+(SIZE+1)+' '+EXTRA)
 
     process01.stdout.on('data', (data) => {
         let log = data.toString().trimStart().trimEnd()
@@ -50,7 +55,7 @@ async function connect01() {
 }
 
 async function connect02() {
-    process02 = exec('node server.js '+(SIZE+2))
+    process02 = exec('node server.js '+(SIZE+2)+' '+EXTRA)
 
     process02.stdout.on('data', (data) => {
         let log = data.toString().trimStart().trimEnd()
@@ -70,7 +75,7 @@ async function connect02() {
 }
 
 async function connect03() {
-    process03 = exec('node server.js '+(SIZE+3))
+    process03 = exec('node server.js '+(SIZE+3)+' '+EXTRA)
 
     process03.stdout.on('data', (data) => {
         let log = data.toString().trimStart().trimEnd()
