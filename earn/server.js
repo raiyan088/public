863816632c0e0b25085f3d6ea4ab35116fb0a887
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2023-12-30 06:13:22 +0600

    Update server.js

--- earn/server.js
+++ earn/server.js
@@ -82,11 +82,11 @@ async function checkClick() {
         await browserStart()
     } else {
         mTimeout++
-        if (mTimeout >= 15) {
+        if (mTimeout >= 30) {
             console.log('----NO-TIME----')
             process.exit(0)
         } else {
-            await delay(10000)
+            await delay(5000)
             console.log('----WAITING----')
             await checkClick()
         }
