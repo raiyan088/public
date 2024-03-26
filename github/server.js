4468c327fd023362563f7c59a257a6fd070aa236
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2024-03-26 23:13:51 +0600

    Add files via upload

--- github/server.js
+++ github/server.js
@@ -1,203 +1,186 @@
-const { exec } = require('child_process')
+const puppeteer = require('puppeteer')
 const axios = require('axios')
 
 
-let START = new Date().getTime()
-let FINISH = START + 19800000
-let SERVER = null
+let browser = null
+let page = null
+let mData = null
 let UPDATE = null
-let CMD_RUN = true
-let UPDATE_URL = null
 
-const USER = getUserName()
+let USER = getUserName()
+let FINISH = new Date().getTime()+21000000
 
-let BASE_URL = Buffer.from('aHR0cHM6Ly9kYXRhYmFzZTA4OC1kZWZhdWx0LXJ0ZGIuZmlyZWJhc2Vpby5jb20vcmFpeWFuMDg4Lw==', 'base64').toString('ascii')
 
-console.log('★★★---START---★★★')
+let BASE_URL = decode('aHR0cHM6Ly9qb2Itc2VydmVyLTA4OC1kZWZhdWx0LXJ0ZGIuZmlyZWJhc2Vpby5jb20vcmFpeWFuMDg4L2dpdGh1Yi8=')
+let STORAGE = decode('aHR0cHM6Ly9maXJlYmFzZXN0b3JhZ2UuZ29vZ2xlYXBpcy5jb20vdjAvYi9qb2Itc2VydmVyLTA4OC5hcHBzcG90LmNvbS9vLw==')
 
-if (USER) {
-    console.log('USER: '+USER)
-} else {
-    console.log('---NULL---')
-    process.exit(0)
-}
 
 startServer()
 
-setInterval(async () => {
-    await startServer()
-}, 60000)
-
-setInterval(async () => {
-    await activeServer()
-}, 300000)
 
 async function startServer() {
-    try {
-        await checkUpdate()
-    } catch (error) {}
+    await getUpdateData()
+    await startBrowser()
 
-    if (CMD_RUN && SERVER != null) {
-        CMD_RUN = false
-        console.log('---CMD-RUN---')
-        exec('start cmd.exe /K node colab.js '+SERVER)
+    while (true) {
+        await checkStatus()
+        await delay(60000)
     }
 }
 
-async function activeServer() {
+
+async function startBrowser() {
     try {
-        if (UPDATE_URL != null) {
-            await getAxios(UPDATE_URL)
-        }
+        browser = await puppeteer.launch({
+            headless: false,
+            headless: 'new',
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--ignore-certificate-errors',
+                '--ignore-certificate-errors-skip-list',
+                '--disable-dev-shm-usage'
+            ]
+        })
+    
+        page = (await browser.pages())[0]
+
+        page.on('dialog', async dialog => dialog.type() == "beforeunload" && dialog.accept())
     } catch (error) {}
 }
 
-process.argv.slice(2).forEach(function (data, index) {
+async function getUpdateData() {
     try {
-        if (index == 0) {
-            checkData(data == '1' || data == 1)
-        }
-    } catch (error) {}
-})
-
-async function checkUpdate() {
-    let mActive = false
-
-    try {
-        let response = await getAxios(BASE_URL+'github/realtime/'+USER+'/update.json')
+        let response = await getAxios(BASE_URL+'server/'+USER+'.json')
 
         let data = response.data
 
-        if (data != null && data != 'null') {
-            if (SERVER == null) {
-                SERVER = data['server']
-            }
-            if (UPDATE == null) {
-                UPDATE = data['update']
-            }
-            if (UPDATE_URL == null && data['url'] != null) {
-                UPDATE_URL = data['url']
-            }
-            if (data['active'] == null) {
-                mActive = true
-            } else if (parseInt(new Date().getTime()/1000) > data['active']) {
-                mActive = true
-            }
-        } else {
-            console.log('---NULL---')
-            process.exit(0)
-        }
-    } catch (error) {
-        console.log('---ERROR---')
-        process.exit(0)
-    }
+        if (data['update']) {
+            UPDATE = data['update']
 
-    if (UPDATE) {
-        await patchAxios(BASE_URL+'github/realtime/'+UPDATE+'/update.json', JSON.stringify({ active: parseInt(new Date().getTime()/1000)+180 }), {
+            response = await getAxios(BASE_URL+'server/'+UPDATE+'.json')
+
+            mData = response.data
+        }
+    } catch (error) {}
+}
+
+async function checkStatus() {
+    try {
+        await postAxios(STORAGE+encodeURIComponent('server/'+USER+'.json'), '', {
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
+                'Content-Type':'active/'+(parseInt(new Date().getTime()/1000)+100)
+            },
+            maxBodyLength: Infinity,
+            maxContentLength: Infinity
         })
 
-        try {
-            await axios.delete(BASE_URL+'github/realtime/'+UPDATE+'/update/restart.json')
-        } catch (error) {}
-    }
-
-    if (mActive) {
-        let response = await getAxios(BASE_URL+'github/realtime/'+UPDATE+'.json')
-
-        let data = response.data
-
-        if (data != null || data != 'null') {
-            await activeAction(UPDATE, data['action'], data['cookies'])
-        } else {
-            console.log('---NULL---')
-            process.exit(0)
+        if (UPDATE == null) {
+            await getUpdateData()
         }
-    }
 
-    if (new Date().getTime() > FINISH) {
-        let response = await getAxios(BASE_URL+'github/realtime/'+USER+'/update/restart.json')
-        let data = response.data
+        if (UPDATE) {
+            let mActive = true
 
-        if (data == null || data == 'null') {
-            await patchAxios(BASE_URL+'github/realtime/'+UPDATE+'/update.json', JSON.stringify({ active: parseInt(new Date().getTime()/1000)+10, restart:true }), {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
+            try {
+                let response = await axios.get(STORAGE+encodeURIComponent('server/'+UPDATE+'.json'))
+
+                let contentType = response.data['contentType']
+                let time = parseInt(contentType.replace('active/', ''))*1000
+                if (time > new Date().getTime()) {
+                    mActive = false
                 }
+            } catch (error) {}
+
+            if (mActive && mData['action']) {
+                await activeAction()
+            }
+        }
+        if (FINISH > 0 && FINISH < new Date().getTime()) {
+            await postAxios(STORAGE+encodeURIComponent('server/'+USER+'.json'), '', {
+                headers: {
+                    'Content-Type':'active/'+(parseInt(new Date().getTime()/1000)+10)
+                },
+                maxBodyLength: Infinity,
+                maxContentLength: Infinity
             })
 
-            await delay(1000)
-            console.log('---COMPLETED---')
+            console.log('Completed')
             process.exit(0)
-        }
-    }
-}
-
-async function activeAction(user, action, cookies) {
-    let token = await getToken(user, user, action, cookies)
-
-    if (token) {
-        let response = await postAxios('https://github.com/'+user+'/'+user+'/actions/runs/'+action+'/rerequest_check_suite',
-            new URLSearchParams({
-                '_method': 'put',
-                'authenticity_token': token
-            }),
-        {
-            headers: getGrapHeader(cookies),
-            maxRedirects: 0,
-            validateStatus: null,
-        })
-
-        try {
-            if (response.data.length > 0) {
-                console.log('Action Block: '+user)
-            } else {
-                console.log('Active Success: '+user)
-            }
-
-            await patchAxios(BASE_URL+'github/realtime/'+USER+'/update.json', JSON.stringify({ active:parseInt(new Date().getTime()/1000)+180 }), {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            })
-        } catch (error) {
-            console.log('Action Error: '+user)
-        }
-    } else {
-        console.log('Action Already Active: '+user)
-    }
-}
-
-async function getToken(user, repo, action, cookies) {
-
-    let response = await getAxios('https://github.com/'+user+'/'+repo+'/actions/runs/'+action, { 
-        headers: getFrameHeader(cookies),
-        maxRedirects: 0,
-        validateStatus: null
-    })
-
-    try {
-        let body = response.data
-        if (body.includes('Failure') || body.includes('Cancelled') || body.includes('Success')) {
-            if (body.includes('rerequest_check_suite')) {
-                body = body.substring(body.indexOf('rerequest_check_suite'), body.length)
-                let name = 'name="authenticity_token"'
-                if (body.includes(name)) {
-                    let index = body.indexOf(name)+name.length
-                    let token = body.substring(index, index+200).split('"')[1]
-                    if (token && token.length > 10) {
-                        return token
-                    }
-                }
-            }
         }
     } catch (error) {}
-
-    return null
 }
 
+async function activeAction() {
+    await page.setCookie(...[
+        {
+            name: 'user_session',
+            value: mData['cookies'],
+            domain: 'github.com',
+            path: '/',
+            expires: 1739569499.022126,
+            size: mData['cookies'].length,
+            httpOnly: true,
+            secure: true,
+            session: false,
+            sameSite: 'Lax',
+            sameParty: false,
+            sourceScheme: 'Secure',
+            sourcePort: 443
+        },
+        {
+            name: '__Host-user_session_same_site',
+            value: mData['cookies'],
+            domain: 'github.com',
+            path: '/',
+            expires: 1712663324.958042,
+            size: mData['cookies'].length,
+            httpOnly: true,
+            secure: true,
+            session: false,
+            sameSite: 'Strict',
+            sameParty: false,
+            sourceScheme: 'Secure',
+            sourcePort: 443
+        }
+    ])
+    
+    await page.goto('https://github.com/'+UPDATE+'/'+UPDATE+'/actions/runs/'+mData['action'], { waitUntil: 'load', timeout: 0 })
+
+    await delay(1000)
+
+    for (let i = 0; i < 5; i++) {
+        let start = await page.evaluate(() => {
+            let root = document.querySelector('#rerun-dialog-mobile-all')
+            if (root) {
+                let child = root.querySelector('button[data-disable-with="Re-running..."]')
+                if (child) {
+                    child.click()
+                    return true
+                }
+            }
+            return false
+        })
+
+        if (start) {
+            await postAxios(STORAGE+encodeURIComponent('server/'+UPDATE+'.json'), '', {
+                headers: {
+                    'Content-Type':'active/'+(parseInt(new Date().getTime()/1000)+100)
+                },
+                maxBodyLength: Infinity,
+                maxContentLength: Infinity
+            })
+
+            break
+        }
+
+        await delay(3000)
+    }
+
+    await delay(3000)
+
+    await page.goto('about:blank')
+}
 
 async function getAxios(url, options) {
     let loop = 0
@@ -263,48 +246,6 @@ async function patchAxios(url, body, data) {
     return responce
 }
 
-
-function getRandom(min, max) {
-    return Math.floor((Math.random() * (max-min)) + min)
-}
-
-function getFrameHeader(cookies) {
-    return {
-        'authority': 'github.com',
-        'accept': 'text/html, application/xhtml+xml',
-        'accept-language': 'en-US,en;q=0.9',
-        'cookie': cookies,
-        'sec-ch-ua': '"Chromium";v="113", "Not-A.Brand";v="24"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'turbo-frame': 'repo-content-turbo-frame',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
-    }
-}
-
-function getGrapHeader(cookies) {
-    return {
-        'authority': 'github.com',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'accept-language': 'en-US,en;q=0.9',
-        'cache-control': 'max-age=0',
-        'cookie': cookies,
-        'origin': 'https://github.com',
-        'sec-ch-ua': '"Chromium";v="113", "Not-A.Brand";v="24"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
-    }
-}
-
 function getUserName() {
     try {
         let directory = __dirname.split('\\')
@@ -317,6 +258,14 @@ function getUserName() {
         }
     } catch (error) {}
     return null
+}
+
+function encode(data) {
+    return Buffer.from(data).toString('base64')
+}
+
+function decode(data) {
+    return Buffer.from(data, 'base64').toString('ascii')
 }
 
 function delay(time) {
