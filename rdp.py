111df2e8d7730c72c0ffd96def17bfe0393c95eb
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2024-11-10 22:14:52 +0600

    Update rdp.py

--- rdp.py
+++ rdp.py
@@ -5,7 +5,7 @@ import subprocess
 try:
     os.system("sudo apt update")
     os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
-    os.system("dpkg --install google-chrome-stable_current_amd64.deb")
-    os.system('apt install --assume-yes --fix-broken')
+    os.system("sudo dpkg --install google-chrome-stable_current_amd64.deb")
+    os.system("sudo apt install --assume-yes --fix-broken")
 except:
     print("Error")
