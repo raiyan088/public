8903380ba39f9583fbf875194a7b7ff1e46e8956
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2024-12-03 22:37:37 +0600

    Update server.js

--- facebook/server.js
+++ facebook/server.js
@@ -1,14 +1,68 @@
 const { exec } = require('node:child_process')
+const express = require('express')
+const os = require('node:os')
 const fs = require('fs')
 
-let ENGINE = 'C:\\ProgramData\\BlueStacks_nxt\\'
-let NAME = 'Pie64'
 
 let ADB = null
+let mId = null
+let mDocker = false
+
+let ENGINE = 'C:\\ProgramData\\BlueStacks_nxt\\'
+let NAME = 'Rvc64'
+
 let USER = getUserName()
 let FINISH = new Date().getTime()+21000000
 
 let STORAGE = decode('aHR0cHM6Ly9maXJlYmFzZXN0b3JhZ2UuZ29vZ2xlYXBpcy5jb20vdjAvYi9qb2Itc2VydmVyLTA4OC5hcHBzcG90LmNvbS9vLw==')
+
+
+let app = express()
+
+app.use(express.json())
+
+app.listen(process.env.PORT || 9099, ()=>{
+    console.log('Listening on port 9099')
+})
+
+app.get('/adb', async (req, res) => {
+    try {
+        if (req.query) {
+            let cmd = req.query.cmd
+            if (cmd) {
+                if(cmd == 'install') {
+                    await adbShell(mId, 'rm -f /sdcard/logged_in')
+                    await adbShell(mId, 'rm -f /sdcard/fb_store.xml')
+                    await adbShell(mId, 'rm -f /sdcard/authentication')
+                    await adbShell(mId, 'pm clear com.facebook.katana')
+                    await adbShell(mId, 'pm grant com.facebook.katana android.permission.READ_CONTACTS')
+                    await adbShell(mId, 'am start com.facebook.katana/com.facebook.katana.LoginActivity')
+                } else if(cmd == 'getFbId') {
+                    if (mDocker) {
+                        await cmdExecute('docker exec -i emulator cp /data/data/com.facebook.katana/shared_prefs/acra_criticaldata_store.xml /sdcard/fb_store.xml')
+                    } else {
+                        await adbShell(mId, '"su -c cp /data/data/com.facebook.katana/shared_prefs/acra_criticaldata_store.xml /sdcard/fb_store.xml"')
+                    }
+                } else if(cmd == 'getAuth') {
+                    if (mDocker) {
+                        await cmdExecute('docker exec -i emulator cp /data/data/com.facebook.katana/app_light_prefs/com.facebook.katana/authentication /sdcard/authentication')
+                    } else {
+                        await adbShell(mId, '"su -c cp /data/data/com.facebook.katana/app_light_prefs/com.facebook.katana/authentication /sdcard/authentication"')
+                    }
+                } else if(cmd.startsWith('getLog_')) {
+                    let id = cmd.replace('getLog_', '')
+                    if (mDocker) {
+                        await cmdExecute('docker exec -i emulator cp /data/data/com.facebook.katana/app_light_prefs/com.facebook.katana/logged_in_'+id+' /sdcard/logged_in')
+                    } else {
+                        await adbShell(mId, '"su -c cp /data/data/com.facebook.katana/app_light_prefs/com.facebook.katana/logged_in_'+id+' /sdcard/logged_in"')
+                    }
+                }
+            }
+        }
+    } catch (error) {}
+    
+    res.end('success')
+})
 
 
 if (USER) {
@@ -25,54 +79,71 @@ setInterval(async () => {
     await checkStatus()
 }, 120000)
 
+
 async function startServer() {
     console.log('Node: ---START-SERVER---')
 
     ADB = await getAdbPlatfrom()
 
+    fs.writeFileSync('url.txt', 'http://'+getIPAddress()+':9099/adb')
+
+    mDocker = await isUseDocker()
+
+    console.log('Node: Docker: '+mDocker)
+
     if (fs.existsSync('localserver')) {
         ENGINE = 'P:\\Program Files\\BlueStacks_nxt\\'
-        NAME = 'Pie64'
+        NAME = 'Rvc64_1'
     }
 
     await checkStatus()
-
-    if (!await isInstallEmulator()) {
-        await waitForInstallEmulator(NAME)
-    }
     
     console.log('Node: Emulator Starting...')
-    
-    await startEmulator(NAME)
-}
 
-async function startEmulator(name) {
-    let mId = await waitForStartEmulator(name, true, '127.0.0.1', 5555)
- 
+    mId = await waitForStartEmulator(NAME, true, '127.0.0.1', 5555)
+
     if (mId) {
         console.log('Node: Device ID: '+mId)
-        
+
         let connected = await waitForDeviceOnline(mId)
 
         if (connected) {
             console.log('Node: Connected Device: '+connected)
 
+            await delay(3000)
+
             let toolsInstall = 0
 
             try {
                 for (let i = 0; i < 15; i++) {
-                    let install = await adbPush(mId, 'Facebook.apk', '/sdcard/Facebook.apk')
-                    if (install) {
+                    let urlPush = await adbPush(mId, 'url.txt', '/sdcard/url.txt')
+                    if (urlPush) {
                         toolsInstall++
-                        console.log('Node: Facebook Import Success')
+                        console.log('Node: Url Push Success')
                         break
                     } else {
-                        console.log('Node: Facebook Import Failed')
+                        console.log('Node: Url Push Failed')
                     }
                     await delay(5000)
                 }
             } catch (error) {
-                console.log('Node: Facebook Import Failed')
+                console.log('Node: Url Push Failed')
+            }
+
+            try {
+                for (let i = 0; i < 15; i++) {
+                    let install = await adbAppInstall(mId, 'Facebook.apk')
+                    if (install) {
+                        toolsInstall++
+                        console.log('Node: Facebook Install Success')
+                        break
+                    } else {
+                        console.log('Node: Facebook Install Failed')
+                    }
+                    await delay(5000)
+                }
+            } catch (error) {
+                console.log('Node: Facebook Install Failed')
             }
 
             try {
@@ -91,10 +162,10 @@ async function startEmulator(name) {
                 console.log('Node: Fb-Creator Install Failed')
             }
 
-            if (toolsInstall >=  2) {
-                await delay(2000)
+
+            if (toolsInstall >= 3) {
                 await startFbCreator(mId)
-        
+
                 try {
                     let prevTime = ''
                     let timeout = 0
@@ -118,7 +189,9 @@ async function startEmulator(name) {
 
                                         if (status[0].trim() == 'true' || status[0].trim() == true) {
                                             permission = true
-                                        } else if (status[1].trim() == 'true' || status[1].trim() == true) {
+                                        }
+
+                                        if (status[1].trim() == 'true' || status[1].trim() == true) {
                                             completed = true
                                         }
                                     } catch (error) {}
@@ -171,32 +244,34 @@ async function startEmulator(name) {
 
                         await delay(1000)
                     }
-                } catch (error) {}   
+                } catch (error) {}
             } else {
                 console.log('Node: All Tools Cannot Installed')
+                await delay(10000)   
             }
         } else {
-            console.log('Node: Device Not-Connect')
+            console.log("Node: Device Can't Connected")
+            await delay(10000)
         }
     } else {
         console.log('Node: Device ID: Null')
+        await delay(10000)
     }
-    
+
     try {
         await postAxios(STORAGE+encodeURIComponent('server/'+USER+'.json'), '', {
             'Content-Type':'active/'+parseInt(new Date().getTime()/1000)
         })
     } catch (error) {}
 
-    console.log('---COMPLETED---')
+    console.log('Node: ---COMPLETED---')
     process.exit(0)
 }
 
 async function startFbCreator(mId) {
     try {
         await adbShell(mId, 'rm -f /sdcard/status.txt')
-        // await adbShell(mId, 'appops set --uid com.rr.fb.creator MANAGE_EXTERNAL_STORAGE allow')
-        await adbShell(mId, 'pm grant com.rr.fb.creator android.permission.READ_EXTERNAL_STORAGE')
+        await adbShell(mId, 'appops set --uid com.rr.fb.creator MANAGE_EXTERNAL_STORAGE allow')
         await adbShell(mId, 'pm grant com.rr.fb.creator android.permission.WRITE_EXTERNAL_STORAGE')
         await adbShell(mId, 'settings put secure enabled_accessibility_services com.rr.fb.creator/com.rr.fb.creator.Accessibility')
         await delay(3000)
@@ -204,62 +279,11 @@ async function startFbCreator(mId) {
     } catch (error) {}
 }
 
-async function waitForInstallEmulator(name) {
-    cmdExecute('Emulator.exe --defaultImageName '+name+' --imageToLaunch '+name)
-
-    await waitForTaskRuning('BlueStacksInstaller.exe')
-    console.log('Node: Open BlueStacks Installer')
-    
-    await cmdExecute('python install.py')
-    
-    console.log('Node: BlueStacks Installing')
-
-    await waitForDeskTopShorcut()
-
-    console.log('Node: BlueStacks Install Success')
-
-    await delay(1000)
-    await cmdExecute('taskkill /IM "BlueStacksInstaller.exe" /T /F')
-    await delay(2000)
-}
-
-async function waitForStartEmulator(name, restart, host, port) {
-    if (restart) {
-        await cmdExecute('taskkill /IM "HD-Player.exe" /T /F')
-        await delay(1000)
-    
-        try {
-            let dixFile = '"'+ENGINE+'Engine\\'+name+'\\Data.vhdx"'
-            let bluestacks = fs.readFileSync('config.conf', 'utf-8')
-            let replaceAdId = await randomAdId(bluestacks)
-            fs.writeFileSync('bluestacks.conf', replaceAdId)
-            await cmdExecute('attrib -r "'+ENGINE+'bluestacks.conf"')
-            await cmdExecute('copy bluestacks.conf  "'+ENGINE+'bluestacks.conf"')
-            await cmdExecute('attrib +r "'+ENGINE+'bluestacks.conf"')
-            await cmdExecute('rm -f '+dixFile)
-            await cmdExecute('copy Data.vhdx '+dixFile)
-        } catch (error) {}
-    
-        await delay(2000)
-        cmdExecute('"C:\\Program Files\\BlueStacks_nxt\\HD-Player.exe" --instance '+name)        
-    }
-    
-    for (let i = 0; i < 60; i++) {
-        try {
-            let result = await cmdExecute(ADB+'connect '+host+':'+port)
-            if (result && (result.indexOf('connected to '+host+':'+port) > -1)) {
-                return host+':'+port
-            }
-        } catch (error) {}
-
-        await delay(1000)
-    }
-}
-
 async function waitForDeviceOnline(d_id) {
     for (let i = 0; i < 60; i++) {
         try {
             let result = await cmdExecute(ADB+'devices')
+            
             let deviceOnline = false
             result.split('\n').forEach(function(line) {
                 try {
@@ -296,47 +320,62 @@ async function waitForDeviceOnline(d_id) {
     return null
 }
 
-async function adbIsInstalled(d_id, pkg) {
-    try {
-        let result = await cmdExecute(ADB+'-s '+d_id+' shell pm path '+pkg)
-        if (result && result.startsWith('package:')) {
-            return true
+async function waitForStartEmulator(name, restart, host, port) {
+    if (restart) {
+        if (!await isInstallEmulator()) {
+            await waitForInstallEmulator(NAME)
         }
-    } catch (error) {}
 
-    return false
+        await cmdExecute('taskkill /IM "HD-Player.exe" /T /F')
+        await delay(1000)
+    
+        try {
+            let dixFile = '"'+ENGINE+'Engine\\'+name+'\\Data.vhdx"'
+            let bluestacks = fs.readFileSync('config.conf', 'utf-8')
+            let replaceAdId = await randomAdId(bluestacks)
+            fs.writeFileSync('bluestacks.conf', replaceAdId)
+            await cmdExecute('attrib -r "'+ENGINE+'bluestacks.conf"')
+            await cmdExecute('copy bluestacks.conf  "'+ENGINE+'bluestacks.conf"')
+            await cmdExecute('attrib +r "'+ENGINE+'bluestacks.conf"')
+            await cmdExecute('rm -f '+dixFile)
+            await cmdExecute('copy Data.vhdx '+dixFile)
+        } catch (error) {}
+    
+        await delay(2000)
+        cmdExecute('"C:\\Program Files\\BlueStacks_nxt\\HD-Player.exe" --instance '+name)        
+    }
+
+    for (let i = 0; i < 60; i++) {
+        try {
+            let result = await cmdExecute(ADB+'connect '+host+':'+port)
+            if (result && (result.indexOf('connected to '+host+':'+port) > -1)) {
+                return host+':'+port
+            }
+        } catch (error) {}
+
+        await delay(1000)
+    }
 }
 
-async function adbAppInstall(d_id, file) {
-    try {
-        let result = await cmdExecute(ADB+'-s '+d_id+' install '+file)
-        if (result && result.includes('Install') && result.includes('Success')) {
-            return true
-        }
-    } catch (error) {}
+async function waitForInstallEmulator(name) {
+    cmdExecute('Emulator.exe --defaultImageName '+name+' --imageToLaunch '+name)
 
-    return false
+    await waitForTaskRuning('BlueStacksInstaller.exe')
+    console.log('Node: Open BlueStacks Installer')
+    
+    await cmdExecute('python install.py')
+    
+    console.log('Node: BlueStacks Installing')
+
+    await waitForDeskTopShorcut()
+
+    console.log('Node: BlueStacks Install Success')
+
+    await delay(1000)
+    await cmdExecute('taskkill /IM "BlueStacksInstaller.exe" /T /F')
+    await delay(2000)
 }
 
-async function adbPush(d_id, file, target) {
-    try {
-        let result = await cmdExecute(ADB+'-s '+d_id+' push '+file+' '+target)
-
-        if (result && result.includes('file pushed')) {
-            return true
-        }
-    } catch (error) {}
-
-    return false
-}
-
-async function adbShell(d_id, cmd) {
-    try {
-        return await cmdExecute(ADB+'-s '+d_id+' shell '+cmd)
-    } catch (error) {}
-
-    return null
-}
 
 async function waitForDeskTopShorcut() {
     for (let i = 0; i < 120; i++) {
@@ -407,22 +446,42 @@ async function cmdExecute(cmd) {
     })
 }
 
-async function postAxios(url, body, data) {
-    return new Promise((resolve) => {
-        try {
-            fetch(url, {
-                method: 'POST',
-                headers: data,
-                body: body
-            }).then((response) => {
-                resolve('ok')
-            }).catch((error) => {
-                resolve('ok')
-            })
-        } catch (error) {
-            resolve('ok')
+async function adbShell(d_id, cmd) {
+    try {
+        return await cmdExecute(ADB+'-s '+d_id+' shell '+cmd)
+    } catch (error) {}
+
+    return null
+}
+
+async function adbAppInstall(d_id, file) {
+    try {
+        let result = await cmdExecute(ADB+'-s '+d_id+' install '+file)
+        if (result && result.includes('Install') && result.includes('Success')) {
+            return true
         }
-    })
+    } catch (error) {}
+
+    return false
+}
+
+async function adbPull(d_id, path) {
+    try {
+        return await cmdExecute(ADB+'-s '+d_id+' pull '+path)
+    } catch (error) {}
+
+    return null
+}
+
+async function adbPush(d_id, file, target) {
+    try {
+        let result = await cmdExecute(ADB+'-s '+d_id+' push '+file+' '+target)
+        if (result && result.includes('file pushed')) {
+            return true
+        }
+    } catch (error) {}
+
+    return false
 }
 
 async function checkStatus() {
@@ -444,12 +503,54 @@ async function checkStatus() {
     }
 }
 
+async function postAxios(url, body, data) {
+    return new Promise((resolve) => {
+        try {
+            fetch(url, {
+                method: 'POST',
+                headers: data,
+                body: body
+            }).then((response) => {
+                resolve('ok')
+            }).catch((error) => {
+                resolve('ok')
+            })
+        } catch (error) {
+            resolve('ok')
+        }
+    })
+}
+
+async function isUseDocker() {
+    try {
+        let result = await cmdExecute('docker --version')
+        if (result) {
+            return true
+        }
+    } catch (error) {}
+
+    return false
+}
+
 async function getAdbPlatfrom() {
     if (fs.existsSync('adb.exe')) {
         return 'adb.exe '
     } else {
         return './adb '
     }
+}
+
+
+function randomHex(size) {
+    let C = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+    
+    let result = ''
+
+    for (let i = 0; i < size*2; i++) {
+        result += C[Math.floor((Math.random() * C.length))]
+    }
+
+    return result
 }
 
 function getUserName() {
@@ -476,16 +577,18 @@ function getUserName() {
     return null
 }
 
-function randomHex(size) {
-    let C = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+function getIPAddress() {
+    let interfaces = os.networkInterfaces()
+    for (let devName in interfaces) {
+        let iface = interfaces[devName]
     
-    let result = ''
-
-    for (let i = 0; i < size*2; i++) {
-        result += C[Math.floor((Math.random() * C.length))]
+        for (let i = 0; i < iface.length; i++) {
+            let alias = iface[i]
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+                return alias.address
+        }
     }
-
-    return result
+    return '0.0.0.0'
 }
 
 function getTimeString(time) {
