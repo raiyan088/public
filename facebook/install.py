01298c2747e89b2fdc82cf9f275e03ab5fb3ec6e
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2025-01-11 14:31:49 +0600

    Update install.py

--- facebook/install.py
+++ facebook/install.py
@@ -3,15 +3,12 @@ from pywinauto import Desktop
 from pywinauto.keyboard import send_keys
 
 time.sleep(1)
-dialog = Desktop()['BlueStacks Installer']
+dialog = Desktop()['MuMu Player 12']
 dialog.wait('visible', timeout=120)
-time.sleep(3)
+
+time.sleep(5)
 send_keys('{TAB}')
-time.sleep(1)
-send_keys('{ENTER}')
-time.sleep(2)
 send_keys('{TAB}')
-time.sleep(1)
 send_keys('{TAB}')
 time.sleep(1)
 send_keys('{ENTER}')
