7756c93bf8f1c319532d99f2b4eb5ed391bdb5fc
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2025-01-14 04:04:19 +0600

    Update install.py

--- facebook/install.py
+++ facebook/install.py
@@ -2,13 +2,14 @@ import time
 from pywinauto import Desktop
 from pywinauto.keyboard import send_keys
 
-time.sleep(1)
-dialog = Desktop()['MuMu Player 12']
-dialog.wait('visible', timeout=120)
+time.sleep(3)
+mDialog = Desktop()['MuMu Player 12 Setup']
+mDialog.wait('visible', timeout=120)
 
-time.sleep(10)
-send_keys('{TAB}')
-send_keys('{TAB}')
-send_keys('{TAB}')
+mNext = mDialog.child_window(title='&Next >', class_name='Button')
+mNext.wait('visible', timeout=15)
+mNext.click()
 time.sleep(1)
-send_keys('{ENTER}')
+mInstall = mDialog.child_window(title='&Install', class_name='Button')
+mInstall.wait('visible', timeout=15)
+mInstall.click()
